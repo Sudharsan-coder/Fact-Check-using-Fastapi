3f3cceaabc5eb6fab3fe936f38ddebba92276935
sudharsan-coder <sudharsan220103@gmail.com>
2023-11-04 10:34:51 +0530

    added try

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ class Question(BaseModel):
 
 @app.get("/")
 async def root():
+    print("root activated")
     return "Welcome to the Question answer api"
 
 @app.post("/answer")
 async def get_answer(Question:Question):
+    print("get_answer activated")
     return findCompleteAnswer(Question.question)
