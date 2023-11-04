7558f7d6fcd24bef67cf7787b37fea1fd6d15151
sudharsan-coder <sudharsan220103@gmail.com>
2023-11-04 19:01:21 +0530

    changed try position

--- Answer.py
+++ Answer.py
@@ -1,27 +1,26 @@
-try:
-  # import venv
-  # import os
-  # venv.create("test")
-  # os.system("source test/bin/activate")
-  # import pip
-  # pip.main(["install", "pandas"])
-  # pip.main(["install", "sentence-transformers"])
-  # pip.main(["install", "torch"])
-  # pip.main(["install", "transformers"])
-  from transformers import AutoModelForSequenceClassification, AutoTokenizer
-  import torch
-  from transformers import pipeline
-  from pandas import *
-  # import sys
-  from sentence_transformers import SentenceTransformer, util
-  # import json
-  from transformers import logging
-  logging.set_verbosity_error()
+# import venv
+# import os
+# venv.create("test")
+# os.system("source test/bin/activate")
+# import pip
+# pip.main(["install", "pandas"])
+# pip.main(["install", "sentence-transformers"])
+# pip.main(["install", "torch"])
+# pip.main(["install", "transformers"])
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+from transformers import pipeline
+from pandas import *
+# import sys
+from sentence_transformers import SentenceTransformer, util
+# import json
+from transformers import logging
+logging.set_verbosity_error()
 
 
-  # data = read_csv("datas.csv")
-  links = ['https://newsmobile.in/articles/2023/10/30/fact-check-ratan-tata-did-not-announce-reward-for-rashid-khan-after-afg-defeated-pak/','https://newsmobile.in/articles/2023/10/28/fact-check-viral-post-claiming-n-koreas-kim-jong-un-criticising-us-for-isreal-palestine-conflict-is-false/']
-  more_details=['''In the midsts of ongoing ICC Men’s Cricket World Cup, a claim surfaced on the internet according to which Industralist Ratan Tata announced a reward of whopping Rs 10 crore for Afghanistan cricketer Rashid Khan. The claim also stated that Pakistan had complain to ICC aganist Rasid Khan during his victory celebration with Indian flag and ICC fined him for Rs 55 lakh aganist Rasid Khan but Ratan Tata declare 10 crore to Rasid Khan.
+# data = read_csv("datas.csv")
+links = ['https://newsmobile.in/articles/2023/10/30/fact-check-ratan-tata-did-not-announce-reward-for-rashid-khan-after-afg-defeated-pak/','https://newsmobile.in/articles/2023/10/28/fact-check-viral-post-claiming-n-koreas-kim-jong-un-criticising-us-for-isreal-palestine-conflict-is-false/']
+more_details=['''In the midsts of ongoing ICC Men’s Cricket World Cup, a claim surfaced on the internet according to which Industralist Ratan Tata announced a reward of whopping Rs 10 crore for Afghanistan cricketer Rashid Khan. The claim also stated that Pakistan had complain to ICC aganist Rasid Khan during his victory celebration with Indian flag and ICC fined him for Rs 55 lakh aganist Rasid Khan but Ratan Tata declare 10 crore to Rasid Khan.
 
 One of the posts read: “Ratan Tata has once again shown his greatness.
 
@@ -76,9 +75,9 @@ A video report by BBC, dated October 13, 2020, shows similar dress as seen in th
 Searching further, our team also found the transcript of the speech made by Kim Jong Un on October 10, 2020, which was translated into English by the Korean Central News Agency. However, there was no mention of the Israel-Palestine conflict in the entire speech. Click here to read the translated speech.
 
 Thus, it is confirmed that the viral video is from 2020 when Kim Jong-Un gave a speech at a military parade. It was NOT related to the ongoing Israel-Hamas conflict.''']
-  summarized_data=['''The claim started doing rounds after Afghanistan defeated Pakistan in the World Cup clash on October 23, 2023. The Afghanistan team took a victory lap of the MA Chidambaram Stadium in Chennai to celebrate their historic win. During the celebration, Rashid Khan could be seen holding the Indian flag.''','''A video of North Korea’s leader Kim Jong-Un speaking about the Israel-Palestine conflict has gone viral on social media. A Facebook user shared this video and wrote: “Guess Who Made A Statement About Israel-Hamas.” NM fact-checked the above claim, and found it to be false.''']
-  def findCompleteAnswer(question):
-
+summarized_data=['''The claim started doing rounds after Afghanistan defeated Pakistan in the World Cup clash on October 23, 2023. The Afghanistan team took a victory lap of the MA Chidambaram Stadium in Chennai to celebrate their historic win. During the celebration, Rashid Khan could be seen holding the Indian flag.''','''A video of North Korea’s leader Kim Jong-Un speaking about the Israel-Palestine conflict has gone viral on social media. A Facebook user shared this video and wrote: “Guess Who Made A Statement About Israel-Hamas.” NM fact-checked the above claim, and found it to be false.''']
+def findCompleteAnswer(question):
+  try:
     model = SentenceTransformer('sentence-transformers/msmarco-distilbert-base-tas-b')
 
     query_emb = model.encode(question)
@@ -105,46 +104,45 @@ Thus, it is confirmed that the viral video is from 2020 when Kim Jong-Un gave a 
       ans= {"validation":boolean_ans,"answer":answer,"link":links[selected_heading]}
     print(ans)
     return ans
+  except Exception as e:
+    return {"error":e}
 
-  def findBooleanAns(doc,question):
-    model = AutoModelForSequenceClassification.from_pretrained("nfliu/roberta-large_boolq")
-    tokenizer = AutoTokenizer.from_pretrained("nfliu/roberta-large_boolq")
-    sample_item=[(question,doc)]
-    encoded_input = tokenizer(sample_item, padding=True, truncation=True, return_tensors="pt")
+def findBooleanAns(doc,question):
+  model = AutoModelForSequenceClassification.from_pretrained("nfliu/roberta-large_boolq")
+  tokenizer = AutoTokenizer.from_pretrained("nfliu/roberta-large_boolq")
+  sample_item=[(question,doc)]
+  encoded_input = tokenizer(sample_item, padding=True, truncation=True, return_tensors="pt")
 
-    with torch.no_grad():
-      model_output = model(**encoded_input)
-      probabilities = torch.softmax(model_output.logits, dim=-1).cpu().tolist()
+  with torch.no_grad():
+    model_output = model(**encoded_input)
+    probabilities = torch.softmax(model_output.logits, dim=-1).cpu().tolist()
 
-    probability_no = [round(prob[0], 2) for prob in probabilities]
-    probability_yes = [round(prob[1], 2) for prob in probabilities]
-    return probability_no<probability_yes
+  probability_no = [round(prob[0], 2) for prob in probabilities]
+  probability_yes = [round(prob[1], 2) for prob in probabilities]
+  return probability_no<probability_yes
 
-  def findAnswer(question,context):
-    model= pipeline("question-answering", model='bert-large-uncased-whole-word-masking-finetuned-squad')
-    result = model(question=question,context=context)
-    if(result['score']<0.1):
-      return None
-    # to get the selected sentence from the passage
-    #   start=result["start"]
-    #   end=result['end']
-    #   while start >=-1:
-    #     if context[start] ==".":
-    #         break
-    #     start -= 1
-    #   while end <= len(context):
-    #     if context[end] == ".":
-    #         break
-    #     end += 1
-    # print("sentence :")
-    # result["answer"]=(context[start+1:end]).strip()
+def findAnswer(question,context):
+  model= pipeline("question-answering", model='bert-large-uncased-whole-word-masking-finetuned-squad')
+  result = model(question=question,context=context)
+  if(result['score']<0.1):
+    return None
+# to get the selected sentence from the passage
+#   start=result["start"]
+#   end=result['end']
+#   while start >=-1:
+#     if context[start] ==".":
+#         break
+#     start -= 1
+#   while end <= len(context):
+#     if context[end] == ".":
+#         break
+#     end += 1
+# print("sentence :")
+# result["answer"]=(context[start+1:end]).strip()
 
-    return (result['answer'])
+  return (result['answer'])
 
-  # question="did ratan tata announce reward for rashid khan?"
-  # ans=findCompleteAnswer(question)
-  # ans=findAssociation(sys.argv[1])
-  # print(json.dumps(ans))
-
-except Exception as e:
-  print(e)
+# question="did ratan tata announce reward for rashid khan?"
+# ans=findCompleteAnswer(question)
+# ans=findAssociation(sys.argv[1])
+# print(json.dumps(ans))
